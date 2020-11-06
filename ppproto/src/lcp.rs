a55d09f0908d067d1c35bd2166ff670454ac4bf8
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2020-11-06 10:56:36 +0100

    Fix asyncmap negotiation, remove magic negotiation.

--- ppproto/src/lcp.rs
+++ ppproto/src/lcp.rs
@@ -1,4 +1,5 @@
 use anyfmt::*;
+use core::convert::TryInto;
 use num_enum::{FromPrimitive, IntoPrimitive};
 
 use super::options::{Protocol, Verdict};
@@ -14,7 +15,6 @@ enum Option {
     Unknown = 0,
     Asyncmap = 2,
     Auth = 3,
-    Magic = 5,
 }
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
@@ -26,16 +26,19 @@ pub enum AuthType {
 
 pub(crate) struct LCP {
     pub auth: AuthType,
+
+    pub asyncmap_remote: u32,
     pub asyncmap: u32,
-    pub magic: u32,
+    pub asyncmap_rej: bool,
 }
 
 impl LCP {
     pub fn new() -> Self {
         Self {
             auth: AuthType::None,
-            asyncmap: 0xFFFFFFFF,
-            magic: 0x00000000,
+            asyncmap_remote: 0xFFFFFFFF,
+            asyncmap: 0x00000000,
+            asyncmap_rej: false,
         }
     }
 }
@@ -54,22 +57,44 @@ impl Protocol for LCP {
         trace!("LCP: rx option {:?} {:?} {:?}", code, opt, data);
         match opt {
             Option::Unknown => Verdict::Rej,
-            Option::Asyncmap => Verdict::Ack,
-            Option::Magic => Verdict::Ack,
-            Option::Auth => {
-                if data.len() != 2 || data != &[0xc0, 0x23] {
-                    return Verdict::Nack(&[0xc0, 0x23]);
+            Option::Asyncmap => {
+                if data.len() == 4 {
+                    self.asyncmap_remote = u32::from_be_bytes(data.try_into().unwrap());
+                    Verdict::Ack
+                } else {
+                    Verdict::Rej
                 }
-                self.auth = AuthType::PAP;
-                Verdict::Ack
+            }
+            Option::Auth => {
+                if data == &[0xc0, 0x23] {
+                    self.auth = AuthType::PAP;
+                    Verdict::Ack
+                } else {
+                    Verdict::Nack(&[0xc0, 0x23])
+                }
             }
         }
     }
 
     fn own_options(&mut self, p: &mut PacketWriter) -> Result<(), Error> {
-        p.append_option(Option::Asyncmap.into(), &[0x00, 0x00, 0x00, 0x00])?;
-        p.append_option(Option::Magic.into(), &[0x12, 0x34, 0x56, 0x78])?;
+        if !self.asyncmap_rej {
+            p.append_option(Option::Asyncmap.into(), &self.asyncmap.to_be_bytes())?;
+        }
         Ok(())
     }
-    fn own_option_nacked(&mut self, _code: u8, _data: &[u8], _is_rej: bool) {}
+
+    fn own_option_nacked(&mut self, code: u8, data: &[u8], is_rej: bool) {
+        let opt = Option::from(code);
+        trace!("LCP nak {:?} {:?} {:?} {:?}", code, opt, data, is_rej);
+        match opt {
+            Option::Asyncmap => {
+                if !is_rej && data.len() == 4 {
+                    self.asyncmap = u32::from_be_bytes(data.try_into().unwrap())
+                } else {
+                    self.asyncmap_rej = true
+                }
+            }
+            _ => {}
+        }
+    }
 }
