1846e6e2cf125f356cce508c8795e35c0e9a858b
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2023-08-24 18:50:51 +0200

    Check docs in CI.

--- README.md
+++ README.md
@@ -43,7 +43,7 @@ and then use `$PWD/pty` instead of `/dev/ttyUSB0`
 This work is licensed under either of
 
 - Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or
-  http://www.apache.org/licenses/LICENSE-2.0)
-- MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+  <http://www.apache.org/licenses/LICENSE-2.0>)
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or <http://opensource.org/licenses/MIT>)
 
 at your option.
--- ci.sh
+++ ci.sh
@@ -3,6 +3,7 @@
 set -euxo pipefail
 
 export RUSTFLAGS=-Dwarnings
+export RUSTDOCFLAGS=-Dwarnings
 
 # std
 cargo build --no-default-features
@@ -12,6 +13,9 @@ cargo build --no-default-features --features log
 cargo build --target thumbv7em-none-eabi --no-default-features
 cargo build --target thumbv7em-none-eabi --no-default-features --features log
 cargo build --target thumbv7em-none-eabi --no-default-features --features defmt
+
+# docs
+cargo doc
 
 # examples
 (cd examples; cargo build --bins)
